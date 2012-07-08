771c64026f763c6901dbe8d7c622ff58bd8e6e1b
Moriyoshi Koizumi <mozo@mozo.jp>
2012-07-09 03:56:32 +0900

    0.0.4pre

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 setup(
     name='tableau',
-    version='0.0.3',
+    version='0.0.4pre',
     author='Moriyoshi Koizumi',
     author_email='mozo@mozo.jp',
     description="Tableau is a collection of helper classes for building test fixtures and seed data",
